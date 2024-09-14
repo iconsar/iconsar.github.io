357aa0876117eab303f3ee360c591d90ef5d0078
Ramiro Alvarez Ugarte <ramiroalvarezugarte@Ramiros-MacBook-Pro-2.local>
2024-09-14 10:31:43 -0300

    Initial

--- deploy.sh
+++ deploy.sh
@@ -11,15 +11,22 @@ hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 # Go To Public folder
 cd public
 
+# Check if the directory is a Git repository
+if [ ! -d ".git" ]; then
+  echo "Not a git repository. Initializing new repository."
+  git init
+  git remote add origin https://github.com/iconsar/iconsar.github.io.git
+fi
+
 # Add changes to git.
 git add .
 
 # Commit changes.
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
-	msg="$*"
+  msg="$*"
 fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git --force push origin main
+git push origin main
